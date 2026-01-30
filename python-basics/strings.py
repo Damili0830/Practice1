13a58a529160b032be5178de65b5db69cd4526ab
Damili Kulbayeva <d_kulbaeva@kbtu.kz>
2026-01-30 14:08:58 +0500

    add pratice 1 pyhton basics examples

---
+++ python-basics/strings.py
@@ -0,0 +1,16 @@
+a = "Hello"
+print(a)
+
+a = "Hello, World!"
+print(a[1])
+
+a = "Hello, World!"
+print(len(a))
+
+txt = "The best things in life are free!"
+if "free" in txt:
+  print("Yes, 'free' is present.")
+
+  txt = "The best things in life are free!"
+  if "expensive" not in txt:
+      print("No, 'expensive' is NOT present.")
